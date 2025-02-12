9aeaec5fc71f0845b6874e331a80ca389a182c32
Masagoro1 <cole@iaff.ca>
2025-02-12 03:00:50 -0800

    Added background colour to global.css

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <body className="bg-orange-100">
+    <body>
       <div class="flex justify-between text-2xl items-center p-4 bg-orange-200 underline decoration-green-900">
         <div class="test-left">
           <span class="font-bold text-green-900">Projects</span>
