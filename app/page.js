83bfd253de17a04f2a53cd5a81f7ebd79d19764a
Masagoro1 <cole@iaff.ca>
2025-02-12 02:41:55 -0800

    added github external link

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <body class="bg-orange-100 text-2xl">
-      <div class="flex justify-between items-center p-4 bg-orange-200 underline decoration-green-900">
+    <body class="bg-orange-100">
+      <div class="flex justify-between text-2xl items-center p-4 bg-orange-200 underline decoration-green-900">
         <div class="test-left">
           <span class="font-bold text-green-900">Projects</span>
         </div>
@@ -16,12 +16,24 @@ export default function Home() {
       </div>
       <h1 class="font-bold text-5xl m-10 text-center text-green-900">Cole McKee</h1>
       <div class="bg-orange-200 flex items-center m-10 rounded-md">
-        <img src="/Green-Cole.jpg" alt="Photo of Cole with green background" class="w-96 m-10 rounded-md"></img>
+        <div class="m-10 w-1/2">
+          <img src="/Green-Cole.jpg" alt="Photo of Cole with green background" class="w-96  rounded-md"></img>
+          <div class="items-center flex pt-5">
+            <a href="https://github.com/Masagoro1" target="_blank" rel="noopener noreferrer">
+              <img src="/github.svg" class="w-12"></img>
+            </a>
+          </div>
+          
+        </div>
+        
+        
         <div class="text-center md:text-left font">
           <h1 class="text-3xl font-bold text-green-900">About Me</h1>
           <p class="text-green-900 text-2xl">Hi my name is Cole McKee. I am a second year CS student at Simon Fraser University. I work as a Coding Instructor for Science Alive. I love doing game development as a hobby. I love to compete in hackathons. I also find teaching very fufilling. My other interests include trading card games like Magic the Gathering, Dungeons and Dragons and Video Games.</p>
         </div>
+        
       </div>
+      
     </body>
   );
 }
