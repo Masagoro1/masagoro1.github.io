b6c8a7c0c39197b40b8bafec75ba01a3530efff7
Masagoro1 <cole@iaff.ca>
2025-02-13 14:55:37 -0800

    added birge adventure to my projects page

--- app/projects/page.js
+++ app/projects/page.js
@@ -22,7 +22,25 @@ export default function Projects(){
                 
                 </div>
             </div>
-            
+            <h1 class="font-bold text-5xl m-10 text-center text-green-900">Projects</h1>
+            <div class="bg-orange-200 flex items-center m-10 rounded-md">
+                
+                    <img src="/birge.png" alt="Photo of Cole with green background" class="w-60  rounded-md m-10"></img>
+                    
+                
+                
+                    
+                    
+                <div class="text-center md:text-left font">
+                    <h1 class="text-3xl font-bold text-green-900 ml-10 mr-10">Birge Adventure</h1>
+                    <p class="text-green-900 text-2xl ml-10 mr-10">
+                        This was a game that I made with my team during FallHacks23.
+                        I was in charge for all the code for the game and using Unity to construct the game. This project won 3rd place.
+                        https://github.com/Masagoro1/FallHacks23
+                    </p>
+                </div>
+                
+            </div>
 
         </body>
     )
