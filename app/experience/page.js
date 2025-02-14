355434cae0255784c67aa0fccd87cc42b666f0d5
Masagoro1 <cole@iaff.ca>
2025-02-13 17:14:29 -0800

    added navigation to experience page

--- app/experience/page.js
+++ app/experience/page.js
@@ -3,7 +3,25 @@ import Link from "next/link"
 export default function Experience(){
     return(
         <body>
-            <h1>Experience</h1>
+            <div class="flex justify-between text-2xl items-center p-4 bg-orange-200 underline decoration-green-900">
+                <div class="test-left">
+                    <Link href="projects">
+                        <span class="font-bold text-green-900">Projects</span>
+                    </Link>
+                </div>
+                <div class="test-center">
+                    <Link href=".">
+                        <span class="font-bold text-green-900">Home</span>
+                    </Link>
+                    
+                </div>
+                <div class="test-right">
+                    <Link href="experience">
+                        <span class="font-bold text-green-900">Experience</span>
+                    </Link>
+                
+                </div>
+            </div>
         </body>
     )
 }
